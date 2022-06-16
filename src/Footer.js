2bceb0cb8c1a5b19d6dd98e07b9649ba9d2c1c62
steven musembi <musembisteven@gmail.com>
2022-06-16 16:31:58 +0300

    modified css

--- src/Footer.js
+++ src/Footer.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { FaGithubSquare, FaTwitterSquare, FaFacebookSquare, FaInstagramSquare, FaLinkedin } from "react-icons/fa"
+import { FaGithubSquare, FaTwitterSquare, FaInstagramSquare, FaLinkedin } from "react-icons/fa"
 
 export default function Footer() {
     return (
         <footer className="footer">
             <div className="icons">
-                <FaTwitterSquare />
-                <FaFacebookSquare />
-                <FaInstagramSquare />
-                <FaLinkedin />
-                <FaGithubSquare />
-            </div>
+                <a href="https://twitter.com/" target="blank"><FaTwitterSquare /></a>
+                <a href="https://www.instagram.com/steven_musembi/" target="blank"><FaInstagramSquare /></a>
 
+                <a href="https://www.linkedin.com/in/stevenmusembi" target="blank"><FaLinkedin /></a>
+                <a href="https://github.com/stevomusembi/" target="blank"><FaGithubSquare /></a>
+            </div>
         </footer>
     )
 }
